3ec0d42a9f53c11451a300939d45d4c0f23d178a
nadja-m <nadja-mueller@gmx.ch>
2016-02-28 17:43:45 +0100

    stroke stärker, passt besser zum Design, musste vor der Legende gemacht werden

--- js/main_all.js
+++ js/main_all.js
@@ -216,7 +216,7 @@ function main_all() {
         .append('path')
           .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
           .attr('stroke', '#F6EAA4')
-          .attr('stroke-width', 1);
+          .attr('stroke-width', 5);
 
           var Usa = raster
             .append("g")
