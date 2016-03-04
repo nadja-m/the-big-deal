f2f5ba211e73c59931bd42a8cc45cb21a7cdb51d
nadja-m <nadja-mueller@gmx.ch>
2016-03-04 15:45:00 +0100

    more cleanup

--- js/_additional.js
+++ js/_additional.js
@@ -1,37 +1,3 @@
-$(document).ready(function(){
-
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-
-
-	//show scroller after some time
-	$('.scroller').delay(2000).fadeIn();
-
-	//initialize scroll
-	$('.scroller').click(function(){
-		$('html, body').animate({scrollTop : 1000},800);
-		return false;
-	});
-
-});
-
-
-
-
-
-
 // function main_all() {
 //   var worldMap;
 //   var syriaCountry;
